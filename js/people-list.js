9ee166685cff8ca657ed37fbd695c463e8028d87
Raniro Coelho <errecme@gmail.com>
2017-08-10 16:02:33 -0300

    Add _populateList and _renderPersonInfo to renderList module

--- js/people-list.js
+++ js/people-list.js
@@ -8,12 +8,24 @@ var renderList = (function() {
   function _startList(data) {
     var peopleNumber = data.count;
     _buildList(peopleNumber);
+    _populateList(peopleNumber);
   }
 
   function _buildList(peopleNumber) {
     for (var i = 0; i < peopleNumber; i++) {
       peopleGrid.appendChild(document.createElement("LI"));
     }
+  }
+
+  function _populateList(peopleNumber) {
+    for (var i = 1; i <= peopleNumber; i++) {
+      currentPersonURL = peopleURL + i;
+      requestSWInfo(currentPersonURL, "GET").then(_renderPersonInfo.bind(null, i), errorHandler);
+    }
+  }
+
+  function _renderPersonInfo(i, data) {
+    peopleGrid.children[i - 1].innerHTML = data.name;
   }
 })();
 
