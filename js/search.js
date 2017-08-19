340e8a112e3b4f46d4cba30c609dd8edd86a2a93
Raniro Coelho <errecme@gmail.com>
2017-08-18 21:55:43 -0300

    Add input style and animation

--- js/search.js
+++ js/search.js
@@ -7,13 +7,18 @@ var search  = (function() {
 
   function _filterMatches() {
     term = searchInput.value;
+    var matchFlag = 0;
     var regex = new RegExp(term, 'gi');
     peopleWrapper.forEach(function(personWrapper) {
       if(personWrapper.children[0].dataset.name) {
-        personWrapper.children[0].dataset.name.match(regex) || personWrapper.children[1].dataset.planet.match(regex) ?
-        personWrapper.classList.add('people-list__item-wrapper--show') :
-        personWrapper.classList.remove('people-list__item-wrapper--show');
+        if (personWrapper.children[0].dataset.name.match(regex) || personWrapper.children[1].dataset.planet.match(regex)) {
+          matchFlag++;
+          personWrapper.classList.add('people-list__item-wrapper--show');
+        } else {
+          personWrapper.classList.remove('people-list__item-wrapper--show');
+        }
       }
     });
+    matchFlag === 0 ? searchInput.classList.add('header__input--no-results') : searchInput.classList.remove('header__input--no-results')
   }
 })();
